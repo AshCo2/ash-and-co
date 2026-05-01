e59a074c7d4b2ede012ec86c88fd959e5163b2f6
AshandCo <damonsashlynn21@gmail.com>
2026-05-01 21:59:45 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,11 @@
-export const metadata = {
-  title: "Ash & Co",
-  description: "Modern cooking with Asian-inspired influence",
-};
-
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, fontFamily: "Arial" }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
